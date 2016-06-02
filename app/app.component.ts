f82ffdb942c24f4eee46a9060c77cc50aba87291
hua <mzhua78@hotmail.com>
2016-06-02 12:24:48 +0800

    first commit,angular2 template

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by hua on 16/6/2.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by hua on 16/6/2.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
